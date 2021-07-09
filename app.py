6330b5a94b75f341d3bdcb9e1fcaa136e9842639
Eric Hanchrow <erich@truveta.com>
2021-07-09 13:44:11 -0700

    Give it a favicon ...
    ... for no particularly good reason (i.e., just to prove that I can)

--- app.py
+++ app.py
@@ -1,8 +1,17 @@
+import pathlib
+
 from flask import Flask
 
 app = Flask(__name__)
+here = pathlib.Path(__file__).parent.absolute()
 
 
 @app.route("/")
 def hello():
     return "Hello, Cleveland!!!"
+
+
+@app.route("/favicon.ico")
+def favicon():
+    with open(here / 'favicon.ico', 'rb') as inf:
+        return inf.read()
